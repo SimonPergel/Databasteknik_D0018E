59a2df96d4fc6c318f562a6193b7ba43cd6c8805
gabaxh-2 <gabaxh-2@student.ltu.se>
2025-02-17 16:29:10 +0100

    Made a checkAdmin method

--- WebServer/index.js
+++ WebServer/index.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:5201/api/mycontroller/insertuser?role=customer&shpcrtid=8&balance=100&acctname=Peter")
+fetch('http://localhost:5201/api/mycontroller/checkadmin?acctname=Bob')
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
@@ -7,26 +7,3 @@ fetch("http://localhost:5201/api/mycontroller/insertuser?role=customer&shpcrtid=
     })
     .then(data => console.log("API Response:", data))
     .catch(error => console.error("API call failed:", error));
-
-//function callDotNetMethod() {
-//    const { getAssemblyExports } = globalThis.getDotnetRuntime(0);
-//    const exports = getAssemblyExports("dbProj.dll"); // Use the assembly name
-  
-//    const message = exports.dotnetapp.Application.updateCarts(5, 3, 6, 9, 1);
-//    console.log(message); // Outputs: Hello from .NET!
-//  }
-
-//  callDotNetMethod();
-
-//DotNet.invokeMethodAsync('updateCarts', 5, 3, 6, 9, 1);
-//pressedOnce = true;
-
-//var buttons = document.getElementById("container");
-//buttons.onclick = onbuttonclicked;
-//pressedOnce = false;
-
-//function onbuttonclicked() {
-//    if (!pressedOnce) {
-
-//    }
-//}
--- WebServer/obj/Debug/net8.0/WebServer.AssemblyInfo.cs
+++ WebServer/obj/Debug/net8.0/WebServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+469ebf0c57420fb2265a9cec38e188944394c331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c1f50bb9d83c51722477d220a83610ea32dc35c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
