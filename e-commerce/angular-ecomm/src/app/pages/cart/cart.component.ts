fdbed865d83e48cac7307acc696a10c7e168c77b
gabaxh <gabriel.axheim@outlook.com>
2025-02-21 09:46:05 +0100

    Added bin and obj to gitignore and made checkout show up on frontend

--- e-commerce/angular-ecomm/src/app/pages/cart/cart.component.ts
+++ e-commerce/angular-ecomm/src/app/pages/cart/cart.component.ts
@@ -1,17 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { CartItemComponent } from './cart-item/cart-item.component';
+import { CheckoutComponent } from "./checkout/checkout.component";
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItemComponent],
+  imports: [CartItemComponent, CheckoutComponent],
   template: `
     <div class="p-6 flex flex-col gap-4">
       <h2 class="text-2xl ">Shopping Cart</h2>
       @for (item of cartService.cart(); track item.id) {
         <app-cart-item [item]="item" />
       }
-
+      <app-checkout />
 
 
     </div>
